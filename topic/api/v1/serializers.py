f32f790dfbe0cc26481d1d5f8b90031354dac0fd
Rodolfo <rodolfojr94@gmail.com>
2022-01-12 19:42:12 -0300

    feat: Created topic view and serializer

---
+++ topic/api/v1/serializers.py
@@ -0,0 +1,34 @@
+###
+# Libraries
+##
+from rest_framework import serializers
+
+###
+# Models
+##
+from topic.models import Topic
+from helpers.serializers import TimestampSerializer
+
+
+class TopicSerializer(TimestampSerializer):
+    author = serializers.PrimaryKeyRelatedField(
+        source="author.username", read_only=True
+    )
+    posts = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+
+    class Meta:
+        model = Topic
+        fields = (
+            "id",
+            "title",
+            "Description",
+            "ulr_name",
+            "author",
+            "posts",
+            "date",
+            "updated",
+        )
+
+    def create(self, validate_data):
+        user = self.context.get("request", {"user", None}).user
+        return Topic.objects.create(**validate_data, author=user)
--- topic/api/v1/views.py
+++ topic/api/v1/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+###
+# Libraries
+###
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+###
+# Serializers
+###
+from topic.api.v1.serializers import TopicSerializer
+
+###
+# Models
+###
+from topic.models import Topic
+
+
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    permission_classes = (permissions.IsAuthenticated,)
