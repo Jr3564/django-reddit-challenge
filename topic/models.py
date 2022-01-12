d1c6cdc2e4dfbccc3a81073968a0f6e36175d222
Rodolfo <rodolfojr94@gmail.com>
2022-01-12 19:41:48 -0300

    feat: Created topic models

--- topic/models.py
+++ topic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from helpers.models import TimestampModel
+
+from accounts.models import User
+from post.models import Post
+
+
+class Topic(TimestampModel):
+    title = models.CharField(max_length=255)
+    Description = models.TextField()
+    ulr_name = models.CharField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    posts = models.ManyToManyField(Post, related_name="topic_posts")
